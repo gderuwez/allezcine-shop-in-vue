0a075006a7cddd8119e2d8bce36f79a2fc53083e
gderuwez <gderuwez@gmail.com>
2018-10-15 11:06:36 +0200

    deploy test

---
+++ dist/precache-manifest.0d7edc73a9bdbf4766221533228698d9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8837c6f069ca0e03871f74b578173cd",
+    "url": "/img/SW-Shirt-04.b8837c6f.jpeg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bdb6ce5f4c29d369fd2794b00cc1b334",
+    "url": "/img/SW-Poster-03.bdb6ce5f.jpg"
+  },
+  {
+    "revision": "55680de9f26fce6da6fc",
+    "url": "/css/chunk-vendors.347fc946.css"
+  },
+  {
+    "revision": "8e76fe7ca190636d38e8fb47e6ba90b4",
+    "url": "/img/SW-Poster-01.8e76fe7c.jpg"
+  },
+  {
+    "revision": "e68ff5b66635c71fd3bd6ac2ac6b4372",
+    "url": "/img/SW-Shirt-01.e68ff5b6.jpg"
+  },
+  {
+    "revision": "5dcb6f032da831f252c1a81fd84f1150",
+    "url": "/img/SW-Poster-04.5dcb6f03.jpeg"
+  },
+  {
+    "revision": "df631eb3dedba6fc347fce71e5c5e499",
+    "url": "/img/SW-Poster-02.df631eb3.jpg"
+  },
+  {
+    "revision": "b89d116ce4b7237b85cbd0564b20162f",
+    "url": "/img/SW-Mug-03.b89d116c.png"
+  },
+  {
+    "revision": "1d298bb7a7f70e4c02a0",
+    "url": "/js/app.81578bba.js"
+  },
+  {
+    "revision": "55680de9f26fce6da6fc",
+    "url": "/js/chunk-vendors.e036936f.js"
+  },
+  {
+    "revision": "aba17f5fab916d551b8a13855f1a76eb",
+    "url": "/img/SW-Mug-01.aba17f5f.jpg"
+  },
+  {
+    "revision": "07d7d373d756c206b84480e620aa757a",
+    "url": "/img/SW-Mug-02.07d7d373.png"
+  },
+  {
+    "revision": "6398a3f9c284210eed1040b4a6cfdaf0",
+    "url": "/img/SW-Mug-04.6398a3f9.jpg"
+  },
+  {
+    "revision": "4cdda9c0632aa8484f71da26c3c91e90",
+    "url": "/img/SW-Shirt-03.4cdda9c0.jpg"
+  },
+  {
+    "revision": "d0880d32c98bfb6d6e7bce51cc02a9cb",
+    "url": "/img/SW-Shirt-02.d0880d32.jpg"
+  },
+  {
+    "revision": "3346ceb4d4ef5e3e837642c3b9814a95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d298bb7a7f70e4c02a0",
+    "url": "/css/app.49cfe4e6.css"
+  }
+];
